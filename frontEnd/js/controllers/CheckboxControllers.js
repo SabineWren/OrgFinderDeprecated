8eade6910ce974e51a42dd29cd3a541b6e35d533
SabineWren <sabinewrensc@gmail.com>
2016-04-14 07:13:54 -0700

    partway done redesigning front-end architecture

--- frontEnd/js/controllers/CheckboxControllers.js
+++ frontEnd/js/controllers/CheckboxControllers.js
@@ -1,76 +1,22 @@
 FrontEndApp.controller('CheckboxOuterController', ['$scope',function($scope) {
 	
+	
+	
+}]);
+
+FrontEndApp.controller('CheckboxCommitmentController', ['$scope', '$http', 'checkboxService', function($scope, $http, checkboxService) {
 	$scope.checkedOuter = 0;
-	
-}]);
-
-/* Activities */
-FrontEndApp.controller('CheckboxActivitiesController', ['$scope', '$http', 'checkChangedService', 'checkboxService', function($scope, $http, checkChangedService, checkboxService) {
-
-	$scope.Category = "Activities";
-	$scope.checkboxModel = checkboxService.query({file: "Activities.json"});
-	$scope.checked = 0;
-	$scope.appliedFilters = [];
-	
-	$scope.callCheckChanged = function(box){
-		checkChangedService.checkChanged(box, $scope);
-	}
-	
-}]);
-
-FrontEndApp.controller('CheckboxArchetypeController', ['$scope', '$http', 'checkChangedService', 'checkboxService', function($scope, $http, checkChangedService, checkboxService) {
-
-	$scope.Category = "Archetype";
-	$scope.checkboxModel = checkboxService.query({file: "Archetype.json"});
-	$scope.checked = 0;
-	$scope.appliedFilters = [];
-	
-	$scope.callCheckChanged = function(box){
-		checkChangedService.checkChanged(box, $scope);
-	}
-	
-}]);
-
-/* Commitment */
-FrontEndApp.controller('CheckboxCommitmentController', ['$scope', '$http', 'checkChangedService', 'checkboxService', function($scope, $http, checkChangedService, checkboxService) {
-	
-	$scope.Category = "Commitment";
-	$scope.checkboxModel = checkboxService.query({file: "Commitment.json"});
-	$scope.checked = 0;
-	$scope.appliedFilters = [];
-	
-	$scope.callCheckChanged = function(box){
-		checkChangedService.checkChanged(box, $scope);
-	}
-	
-}]);
-
-/* Recruiting */
-FrontEndApp.controller('CheckboxRecruitingController', ['$scope', '$http', 'checkChangedService', 'checkboxService', function($scope, $http, checkChangedService, checkboxService) {
-	
-	$scope.Category = "Recruiting";
-	
-	$scope.checkboxModel = checkboxService.query({file: "Recruiting.json"});
-	$scope.checked = 0;
-	$scope.appliedFilters = [];
-	
-	$scope.callCheckChanged = function(box){
-		checkChangedService.checkChanged(box, $scope);
-	}
-	
-}]);
-
-/* Recruiting */
-FrontEndApp.controller('CheckboxRolePlayController', ['$scope', '$http', 'checkChangedService', 'checkboxService', function($scope, $http, checkChangedService, checkboxService) {
-	
-	$scope.Category = "Role Play";
-	
-	$scope.checkboxModel = checkboxService.query({file: "RolePlay.json"});
-	$scope.checked = 0;
-	$scope.appliedFilters = [];
-	
-	$scope.callCheckChanged = function(box){
-		checkChangedService.checkChanged(box, $scope);
-	}
+	$scope.category = "Commitment";
+	$scope.checkboxModels = [
+		checkboxService.query({file: "Commitment.json"}),
+		checkboxService.query({file: "Activities.json"}),
+		checkboxService.query({file: "Archetype.json"}),
+		checkboxService.query({file: "RolePlay.json"})
+		/*checkboxService.query({file: "Activities.json"})
+		checkboxService.query({file: "Activities.json"})
+		checkboxService.query({file: "Activities.json"})
+		checkboxService.query({file: "Activities.json"})*/
+	]
+	$scope.allAppliedFilters = [];
 	
 }]);
--- frontEnd/js/directives/printCheckbox.js
+++ frontEnd/js/directives/printCheckbox.js
@@ -1,7 +1,30 @@
-FrontEndApp.directive('printCheckbox', function() { 
+FrontEndApp.directive('printCheckbox', function() {
+
   return { 
-    restrict: 'E', 
-    scope: false, 
+    restrict: 'E',
+    require: '^ngModel',
+    
+    scope: true,
+	
+	controller: function($scope){
+		$scope.checked = 0;
+		$scope.appliedFilters = [];
+		$scope.$parent.checkedOuter = 50;
+		
+		$scope.callCheckChanged = function(box) {
+			if(box.isSelected){
+				$scope.$parent.checkedOuter++;
+				$scope.checked++;
+				$scope.appliedFilters.push(box.name);
+			}
+			else{
+				$scope.$parent.checkedOuter--;
+				$scope.checked--;
+				$scope.appliedFilters.splice($scope.appliedFilters.indexOf(box.name), 1);
+			}
+		};
+	},
+    
     templateUrl: 'frontEnd/js/directives/printCheckbox.html'
   }; 
 });
