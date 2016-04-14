980f8b843edf88364fc342bf5220f6cdd7de0c8f
SabineWren <sabinewrensc@gmail.com>
2016-04-14 07:54:19 -0700

    fixed the outer count bug plus some other minor changes

--- frontEnd/js/controllers/Controllers.js
+++ frontEnd/js/controllers/Controllers.js
@@ -1,21 +1,16 @@
 FrontEndApp.controller('MainController', ['$scope',function($scope) {
 	
-	
-	
 }]);
 
 FrontEndApp.controller('CheckboxController', ['$scope', '$http', 'checkboxService', function($scope, $http, checkboxService) {
-	$scope.checkedOuter = 0;
+	$scope.checkedOuter = {num: 0};
 	$scope.category = "Commitment";
 	$scope.checkboxModels = [
 		checkboxService.query({file: "Commitment.json"}),
-		checkboxService.query({file: "Activities.json"}),
+		checkboxService.query({file: "RolePlay.json"}),
 		checkboxService.query({file: "Archetype.json"}),
-		checkboxService.query({file: "RolePlay.json"})
-		/*checkboxService.query({file: "Activities.json"})
-		checkboxService.query({file: "Activities.json"})
-		checkboxService.query({file: "Activities.json"})
-		checkboxService.query({file: "Activities.json"})*/
+		checkboxService.query({file: "Activities.json"}),
+		checkboxService.query({file: "Recruiting.json"})
 	]
 	$scope.allAppliedFilters = [];
 	
--- frontEnd/js/directives/printCheckbox.js
+++ frontEnd/js/directives/printCheckbox.js
@@ -2,26 +2,27 @@ FrontEndApp.directive('printCheckbox', function() {
 
   return { 
     restrict: 'E',
-    require: '^ngModel',
     
     scope: true,
 	
 	controller: function($scope){
 		$scope.checked = 0;
 		$scope.appliedFilters = [];
-		$scope.$parent.checkedOuter = 50;
 		
 		$scope.callCheckChanged = function(box) {
 			if(box.isSelected){
-				$scope.$parent.checkedOuter++;
+				$scope.$parent.checkedOuter.num++;
 				$scope.checked++;
 				$scope.appliedFilters.push(box.name);
+				//$scope.$parent.allAppliedFilters.push(box.name);
 			}
 			else{
-				$scope.$parent.checkedOuter--;
+				$scope.$parent.checkedOuter.num--;
 				$scope.checked--;
 				$scope.appliedFilters.splice($scope.appliedFilters.indexOf(box.name), 1);
+				//$scope.$parent.allAppliedFilters.splice($scope.appliedFilters.indexOf(box.name), 1);
 			}
+			console.log($scope.$parent.checkedOuter);
 		};
 	},
     
