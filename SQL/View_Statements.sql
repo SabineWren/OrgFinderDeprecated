8c39ddc306b075dd67213e26ab4af25f276eb7f1
SabineWren <sabinewrensc@gmail.com>
2016-04-16 23:09:39 -0700

    started joining views

--- SQL/View_Statements.sql
+++ SQL/View_Statements.sql
@@ -1,8 +1,19 @@
-CREATE VIEW OrganizationsFullView as
-SELECT SID, Name, Icon, Commitment
+CREATE VIEW View_Roleplaying as
+SELECT SID, CASE
+	WHEN rpr.Organization IS NOT NULL then "Yes"
+	ELSE "No"
+	END AS Rolplay
+FROM tbl_Organizations orgs
+LEFT JOIN tbl_RolePlayOrgs rpr
+on orgs.SID = rpr.Organization;
+
+CREATE VIEW View_OrganizationsEverything as
+SELECT orgs.SID, Name, Icon, Commitment, Rolplay
 FROM tbl_Organizations orgs
 LEFT JOIN tbl_Commits cr
-ON orgs.SID = cr.Organization;
+ON orgs.SID = cr.Organization
+JOIN view_Roleplaying
+ON orgs.SID = view_Roleplaying.SID;
 
-SELECT *
-FROM OrganizationsFullView;
+
+
