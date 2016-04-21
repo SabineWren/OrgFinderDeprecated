ca7fb0b1d952d1e849548c0d9c37bae5784f8389
LucFauvel <luc.fauvel@hotmail.com>
2016-04-21 01:29:27 -0400

    Arrange json parsing but can't loop

--- backEnd/jsonparse.php
+++ backEnd/jsonparse.php
@@ -1,32 +1,24 @@
 <?php
     //copied from http://www.kodingmadesimple.com/2014/12/how-to-insert-json-data-into-mysql-php.html
-    $connect = mysql_connect("username","password","") or die('Could not connect: ' . mysql_error());
-
-    mysql_select_db("employee", $connect);
+    $connect = mysqli_connect("localhost","root","", "cognitiondb") or die('Could not connect: ' . mysqli_error());
 
     //read the json file contents
-    $jsondata = file_get_contents('');
     
-    //convert json object to php associative array
-    $data = json_decode($jsondata, true);
-    
-    //put info into variables *example*
-    $id = $data['empid'];
-    $name = $data['personal']['name'];
-    $gender = $data['personal']['gender'];
-    $age = $data['personal']['age'];
-    $streetaddress = $data['personal']['address']['streetaddress'];
-    $city = $data['personal']['address']['city'];
-    $state = $data['personal']['address']['state'];
-    $postalcode = $data['personal']['address']['postalcode'];
-    $designation = $data['profile']['designation'];
-    $department = $data['profile']['department'];
-    
-    //insert into mysql table
-    $sql = "INSERT INTO tbl_emp(empid, empname, gender, age, streetaddress, city, state, postalcode, designation, department)
-    VALUES('$id', '$name', '$gender', '$age', '$streetaddress', '$city', '$state', '$postalcode', '$designation', '$department')";
-    if(!mysql_query($sql,$connect))
-    {
-        die('Error : ' . mysql_error());
-    }
+    $lines = file('orgs.txt');
+    //foreach ($lines as $org)
+    //{
+        $jsondata = file_get_contents('http://sc-api.com/?api_source=cache&start_date=&end_date=&system=organizations&action=single_organization&target_id=COGNITION&format=pretty_json');
+        //convert json object to php associative array
+        $data = json_decode($jsondata, true);
+        
+        //put info into variables *example*
+        $sid = $data['data']['sid'];
+        $name = $data['data']['title'];
+        $icon = $data['data']['logo'];
+        
+        //insert into mysql table
+        $sql = "INSERT INTO tbl_Organizations(SID, Name, Icon)
+        VALUES('$sid', '$name', '$icon')";
+        mysqli_query($connect, $sql);
+   // }
 ?>
