c338df938de5814f369aceacdbead8877cd5f756
SabineWren <sabinewrensc@gmail.com>
2016-04-24 13:02:28 -0700

    fixed member listing for all of cog

--- frontEnd/js/controllers/MemberController.js
+++ frontEnd/js/controllers/MemberController.js
@@ -1,4 +1,21 @@
 FrontEndApp.controller('MemberController', ['$scope', '$http', 'readFileService', 'getMembersService', function($scope, $http, readFileService, getMembersService) {
-	$scope.orgData = readFileService.query({file: "orgList.json"});
+	
+	$scope.orgDataArray = [];
+	
+	var query = readFileService.query({file: "orgList.json"});
+	query.$promise.then(function(data){
+		$scope.orgData = data;
+		
+		for(var org in $scope.orgData){
+			if(!isNaN(org)){//ignore promise and resolved
+				var results = getMembersService.get({orgName: $scope.orgData[org].SID});
+				results.$promise.then(function(apiObject){
+					for(member in apiObject.data){
+						$scope.orgDataArray.push(apiObject.data[member].handle);
+					}
+				});//end members subquery
+			}
+		}
+	});//end orgList query
 }]);
 
