9f814b9b912597318fa4900b826d7a754ff66578
LucFauvel <luc.fauvel@hotmail.com>
2016-04-24 21:11:06 -0400

    Moved to use all orgs still offset errors

--- backEnd/jsonparse.php
+++ backEnd/jsonparse.php
@@ -1,24 +1,36 @@
 <?php
     //copied from http://www.kodingmadesimple.com/2014/12/how-to-insert-json-data-into-mysql-php.html
-    $connect = mysqli_connect("localhost","root","", "cognitiondb") or die('Could not connect: ' . mysqli_error());
+    //$connect = mysqli_connect("64.71.77.121","overclock","Cogintstr07", "cognitiondb") or die('Could not connect: ' . mysqli_error());
 
     //read the json file contents
-    
-    $lines = file('orgs.txt');
-    //foreach ($lines as $org)
-    //{
-        $jsondata = file_get_contents('http://sc-api.com/?api_source=cache&start_date=&end_date=&system=organizations&action=single_organization&target_id='.$org.'&format=pretty_json');
+    for($x = 1; $x <= 1160; $x++)
+    {
+        $y = 0;
+        $lines = file_get_contents('http://sc-api.com/?api_source=live&system=organizations&action=all_organizations&source=rsi&start_page='.$x.'&end_page='.$x.'&items_per_page=255&sort_method=&sort_direction=ascending&expedite=0&format=pretty_json');
         //convert json object to php associative array
-        $data = json_decode($jsondata, true);
+        $data = json_decode($lines, true);
+        foreach ($data as $org)
+        {
+          foreach($org)
+           {
+            //put info into variables
+            $sid = $org['sid'];
+            $name = $org['title'];
+            $icon = $org['logo'];
+           }
+            
         
-        //put info into variables *example*
-        $sid = $data['data']['sid'];
-        $name = $data['data']['title'];
-        $icon = $data['data']['logo'];
+            echo "$sid \n";
+            echo "$name \n";
+            echo "$icon \n";
         
-        //insert into mysql table
-        $sql = "INSERT INTO tbl_Organizations(SID, Name, Icon)
-        VALUES('$sid', '$name', '$icon')";
-        mysqli_query($connect, $sql);
-   // }
-?>
+            //insert into mysql table
+            //$sql = "INSERT INTO tbl_Organizations(SID, Name, Icon)
+            //VALUES('$sid', '$name', '$icon')";
+            //mysqli_query($connect, $sql);
+       }
+       
+       unset($lines);
+       unset($data);
+    }
+?>  
