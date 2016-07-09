1e4eacab5b79b0f535b0ee0533c7790083459953
SabineWren <sabinewrensc@gmail.com>
2016-07-09 09:43:34 -0700

    added more license info and deleted unused files

--- frontEnd/js/directives/printCheckbox.js
+++ frontEnd/js/directives/printCheckbox.js
@@ -1,3 +1,14 @@
+/*
+	@license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt
+	
+	Copyright (C) 2016 LucFauvel and SabineWren
+	
+	GNU AFFERO GENERAL PUBLIC LICENSE Version 3, 19 November 2007
+	https://www.gnu.org/licenses/license-list.en.html#AGPL
+	
+	@license-end
+*/
+
 FrontEndApp.directive('printCheckbox', function() {
 
 	return { 
