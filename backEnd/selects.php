52a12642a46b5783023721de3e7939673a0228a1
SabineWren <sabinewrensc@gmail.com>
2016-08-02 01:57:57 -0700

    fully implemented all filters that currently have checkboxes and swapped icon/text order on browser table

--- backEnd/selects.php
+++ backEnd/selects.php
@@ -8,9 +8,33 @@
 	https://www.gnu.org/licenses/agpl-3.0.html
 	
 	@license-end
-	*/
+
+CAVEATS:
+Activity (front end) == Focus (database)
+
+SELECTION Query Types:
+
+	Default:
 	
-	//THIS DOES NOT WORK WITH ACTIVITIES AND IT'S THE WRONG TABLE FOR FILTERING THEM ANYWAY
+	SELECT * FROM View_OrganizationsEverything WHERE ATTRIBUTE1 = ?1 OR Attribute1 = ?2 OR ... OR AttributeN = ?M LIMIT 10 OFFSET ?
+	"s^{M}d", $value1, $value2, ..., $valueM, $pagenum
+	//M 's's and 1 d
+	
+	If filter by Activity, then as above with additions:
+	
+	SELECT * FROM View_OrganizationsEverything WHERE ATTRIBUTE1 = ?1 OR Attribute1 = ?2 OR ... OR AttributeN = ?M
+	<WHERE or AND> SID IN (
+		SELECT SID FROM View_OrgsFilterPrimary
+		WHERE Activity = ?a OR ... OR Activity = ?z
+	)
+	OR SID IN (
+		SELECT SID from View_OrgsFilterSecondary
+		WHERE Activity = ?a OR ... OR Activity = ?z
+	)
+	LIMIT 10 OFFSET ?;
+	"s^{M+2*z}d", $value1, $value2, ..., $valueM, $valuea ... $valuez, $valuea ... $valuez, $pagenum
+	//M+2*z 's's and 1 d
+	*/
 	$connection = new mysqli("192.168.0.105","publicselect","public", "cognitiondb");
 	if( mysqli_connect_errno() ){
 		die( "Connection failed: " . mysqli_connect_error() );
@@ -23,24 +47,53 @@
 		exit("invalid page offset"); 
 	}
 	
+	$UseActivityFilter = false;
+	$Activities = explode( ',', $_GET['Activity'] );
+	if(strlen($Activities[0]) > 0)$UseActivityFilter = true;
+	
 	$Attributes = ['Archetype', 'Commitment', 'Recruiting', 'Roleplay'];
 	
 	//init loop
 	$sql = "SELECT * FROM View_OrganizationsEverything";
 	$parameters = array();
 	$types = '';
-	$or = ' WHERE ';
+	$conjunction = ' WHERE ';
 	
-	//dynamically add select restrictions
-	foreach($Attributes as $Attribute){
-		$Values = explode( ',', $_GET[$Attribute] );
+	function addParamsToQuery($Attribute, $Values, &$types, &$sql, &$conjunction, &$parameters){
 		foreach($Values as $Value){
-			if($Value == '')continue;
-			$sql .= $or . $Attribute . ' = ?';
+			if($Value == '')continue;//We might have 0 Params to add
+			$sql .= $conjunction . $Attribute . ' = ?';
 			array_push($parameters, $Value);//need array of references
 			$types .= 's';
-			$or = ' OR ';
+			$conjunction = ' OR ';
 		}
+	}
+	
+	//dynamically add query restrictions	
+	foreach($Attributes as $Attribute){
+		$Values = explode( ',', $_GET[$Attribute] );
+		addParamsToQuery($Attribute, $Values, $types, $sql, $conjunction, $parameters);
+		$conjunction = ' AND ';
+	}
+	
+	
+	if($UseActivityFilter){
+		//there could be other query restrictions
+		if( sizeof($parameters) === 0)$sql .= ' WHERE ';
+		else $sql .= ' AND ';
+		
+		$sql .= '( ';
+			//add filter and join for primary focus (activity1)
+			$sql .= 'SID IN (SELECT SID FROM View_OrgsFilterPrimary';
+			$conjunction = ' WHERE ';
+			addParamsToQuery('Focus', $Activities, $types, $sql, $conjunction, $parameters);
+		
+			//add filter and join for secondary focus (activity2)
+			$sql .= ' ) OR SID IN (SELECT SID from View_OrgsFilterSecondary';
+			$conjunction = ' WHERE ';
+			addParamsToQuery('Focus', $Activities, $types, $sql, $conjunction, $parameters);
+			$sql .= ') ';
+		$sql .= ')';
 	}
 	
 	//add offset
@@ -61,11 +114,6 @@
 	call_user_func_array( array($prepared_select, "bind_param"), $bindParams );
 	//var_dump($sql, $types, $bindParams, $connection->error);
 	//var_dump($prepared_select);
-	
-	//THIS WAS CODE WITHOUT FILTERING
-	//$prepared_select->close();
-	//$prepared_select = $connection->prepare("SELECT * FROM View_OrganizationsEverything LIMIT 10 OFFSET ?");
-	//$prepared_select->bind_param("d", $pagenum);
 	
 	$pagenum = $pagenum * 10;//number of results to skip
 	$prepared_select->execute();
